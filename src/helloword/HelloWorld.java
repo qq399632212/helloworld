df25308543b4017c50498c1a3a9f69df642baa57
Administrator <Administrator@LiaoJunJie>
2020-09-25 10:39:05 +0800

    test2

--- src/helloword/HelloWorld.java
+++ src/helloword/HelloWorld.java
@@ -3,6 +3,6 @@ package helloword;
 public class HelloWorld {
  
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World 随便改一改");
     }
 }
